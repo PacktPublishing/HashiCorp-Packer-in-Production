2bbb9edce88f964fa9984d46891712eaa02a7ba0
Rickard von Essen <rickard.von.essen@gmail.com>
2022-05-18 14:03:19 +0200

    Add some instructions on how to use the vaultsample.sh file

--- Chapter03/vaultsample.sh
+++ Chapter03/vaultsample.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+# Source this file before running packer to setup your Valt environment variables.
+# 
+#   . vaultsample.sh 
 
 export VAULT_ADDR=https://yourvault:8200
 export VAULT_TOKEN=[YOURTOKEN]
